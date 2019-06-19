da94600fd6bf86e733718918241e0b9de7aa990b
Amanda Dovel <amandacdovel@gmail.com>
2019-06-19 14:02:09 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Dub Cheesin Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veg Burger');
+INSERT INTO burgers (burger_name) VALUES ('Shroom Swiss Burger');
